96785072f7ba3e5b87d6a3fe70b0182abe1a61ca
Koo1433 <s19t15908@stu.edu.tw>
2021-03-26 14:14:23 +0800

    commit

---
+++ 2/4.7.php
@@ -0,0 +1,12 @@
+<?php
+$sum = 0;
+$i = 0;
+
+while ($i <= 60){
+    $sum = $sum +$i;
+    $i++;
+
+}
+
+printf("总计: %d",$sum);
+?>
---
+++ 2/4.8.php
@@ -0,0 +1,13 @@
+<?php
+$sum = 0;
+$i = 0;
+
+do {
+    $i++;
+    $sum = $sum +$i;
+    
+
+} while($i < 50);
+
+printf("总计: %d",$sum);
+?>
